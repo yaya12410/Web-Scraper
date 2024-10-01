192f32cca4b7395743add4910c1cdda5d17c08dd
yaya12410 <ymabdelbarr@gmail.com>
2024-10-02 02:02:16 +0300

    Not a big progress but found out what do I need

--- Request.py
+++ Request.py
@@ -1,1 +1,11 @@
-print("hellooooo")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://github.com/yaya12410" #The link to the page to be scraped
+response = requests.get(url) #Get request to reach out to the server
+html_code=response.content.decode()
+# print(html_code)
+soup = BeautifulSoup(html_code, 'html.parser')
+data = soup.find('tbody')
+print(data)
+
